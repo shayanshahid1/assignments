52cdf5e946c532a3874883b46ac2f525f8776d4c
shayanshahid1 <139454860+shayanshahid1@users.noreply.github.com>
2023-07-27 12:48:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+
+
+var x=prompt("any number")
+
+ if (x % 2 == 0){
+  alert("even")
+ }
+
+
+ else
+ {
+   alert ("odd")
+ }
+
+
+ var area
+ r=prompt("enter radius of circle")
+ area= 3.141*r*r;
+ alert("area of circle "+area)
+
+ var w=3,h=6,area;
+ w=prompt("area of rectangle")
+ area=w*h;
+ alert("area pf rectangle is "+ area)
+
+
+
+
+
+
+
